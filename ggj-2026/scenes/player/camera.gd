138b9fa41eb59d9a7817acc1b025c1ce07378cec
nyk <thenykreed@gmail.com>
2026-01-31 01:19:39 -0600

    controller controls

--- ggj-2026/scenes/player/camera.gd
+++ ggj-2026/scenes/player/camera.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
 @export var sens: Vector2 = Vector2(0.01, 0.01)
+@export var csens: Vector2 = Vector2.ONE * 5.0
+@onready var rot_x: Node3D = $RotX as Node3D
 
 var _last_mouse_motion: Vector2 = Vector2.ZERO
 
@@ -12,6 +14,14 @@ func _input(event: InputEvent) -> void:
 
 func _process(delta: float) -> void:
 	rotate_y(_last_mouse_motion.x * -sens.x)
-	$RotX.rotate_x(_last_mouse_motion.y * -sens.y)
-	$RotX.rotation_degrees.x = clampf($RotX.rotation_degrees.x, -50, 70)
+	rot_x.rotate_x(_last_mouse_motion.y * -sens.y)
+	rot_x.rotation_degrees.x = clampf(rot_x.rotation_degrees.x, -50, 70)
+	controller_camera(delta)
 	_last_mouse_motion = Vector2.ZERO
+
+
+func controller_camera(delta: float) -> void:
+	_last_mouse_motion = Input.get_vector(&"camera_left", &"camera_right", &"camera_up", &"camera_down") * delta
+	rotate_y(_last_mouse_motion.x * -csens.x)
+	rot_x.rotate_x(_last_mouse_motion.y * -csens.y)
+	rot_x.rotation_degrees.x = clampf(rot_x.rotation_degrees.x, -50, 70)
