8798995fa99e50f397a97db074e220c922a34035
nyk <thenykreed@gmail.com>
2026-01-31 23:18:59 -0600

    new shotgun, camera angles increased

--- ggj-2026/scenes/player/camera.gd
+++ ggj-2026/scenes/player/camera.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@export var sens: Vector2 = Vector2(0.01, 0.01)
+@export var sens: Vector2 = Vector2(0.01, 0.125)
 @export var csens: Vector2 = Vector2.ONE * 5.0
 @onready var rot_x: Node3D = $RotX as Node3D
 
@@ -14,8 +14,7 @@ func _input(event: InputEvent) -> void:
 
 func _process(delta: float) -> void:
 	rotate_y(_last_mouse_motion.x * -sens.x)
-	rot_x.rotate_x(_last_mouse_motion.y * -sens.y)
-	rot_x.rotation_degrees.x = clampf(rot_x.rotation_degrees.x, -50, 70)
+	rot_x.rotation_degrees.x = clampf(rot_x.rotation_degrees.x + (_last_mouse_motion.y * -sens.y) * PI, -85, 85)
 	controller_camera(delta)
 	_last_mouse_motion = Vector2.ZERO
 
@@ -24,4 +23,4 @@ func controller_camera(delta: float) -> void:
 	_last_mouse_motion = Input.get_vector(&"camera_left", &"camera_right", &"camera_up", &"camera_down") * delta
 	rotate_y(_last_mouse_motion.x * -csens.x)
 	rot_x.rotate_x(_last_mouse_motion.y * -csens.y)
-	rot_x.rotation_degrees.x = clampf(rot_x.rotation_degrees.x, -50, 70)
+	rot_x.rotation_degrees.x = clampf(rot_x.rotation_degrees.x, -85, 85)
